ee73e5627ed915a388de0bc2ca14f3bf5b2288f2
Vladyslav Babych <88340725+vladyslav-babych@users.noreply.github.com>
2026-03-15 19:31:23 +0200

    Update environment variable for Discord token

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import tickets
 load_dotenv()
 
 
-token = os.getenv('DISCORD_TOKEN_TEST')
+token = os.getenv('DISCORD_TOKEN')
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
